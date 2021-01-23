6adbac0da1105650fdc230799eb0ec556a3b2b31
Sébastien <seb.lasante@hotmail.com>
2021-01-23 14:42:16 +0100

    start

---
+++ public/js/main.js
@@ -0,0 +1,32 @@
+let personne = {
+    nom : "",
+    lieu : "",
+    argent: 0,
+    mainDroite: [],
+    mainGauche: [],
+    seDeplacer(){
+
+    },
+    payerArticle(){
+
+    },
+    couper() {
+
+    },
+}
+
+
+let maison = {
+    nom: "maison",
+    personnes: []
+}
+
+
+
+let couteau={
+    nom = "couteau",
+    action = "coupé"
+}
+
+
+let maison = new Lieu("maison",[])
